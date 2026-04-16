c2bead60d2788496ac8b5bd1c7999e4178ce7b99
codebytewithshobha <kumari3103shobha@gmail.com>
2026-04-16 17:05:50 +0530

    Update app base and router basename for Vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/smart-trip-weaver-68">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/results" element={<ResultsPage />} />
